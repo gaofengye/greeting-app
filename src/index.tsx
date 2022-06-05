4d64feb2eb634aa3132fd3341662166995a45b3e
Gao <gao@hypecode.com>
2022-06-05 20:05:37 +0200

    Feat: firebase added

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import './index.scss';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -7,6 +9,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCtZ5MlHSl-QpvQw_jKHAANotdCPXxWlq8',
+  authDomain: 'greeting-app-8c6b1.firebaseapp.com',
+  projectId: 'greeting-app-8c6b1',
+  storageBucket: 'greeting-app-8c6b1.appspot.com',
+  messagingSenderId: '765826247542',
+  appId: '1:765826247542:web:af536c64498c6ddd88e857',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 root.render(
   <React.StrictMode>
     <App />
